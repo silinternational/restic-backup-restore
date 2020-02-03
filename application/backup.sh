0eedb602baa9f0a1e25a3823ebfe27374b8838a0
Dale Newby <dale_newby@sil.org>
2020-02-03 14:31:43 -0500

    Remove extra shell invocation.

--- application/backup.sh
+++ application/backup.sh
@@ -3,19 +3,19 @@
 logger -p user.info "Started backup..."
 
 start=$(date +%s)
-runny $(/usr/local/bin/restic backup ${RESTIC_BACKUP_ARGS} ${SOURCE_PATH})
+runny /usr/local/bin/restic backup ${RESTIC_BACKUP_ARGS} ${SOURCE_PATH}
 end=$(date +%s)
 
 logger -p user.info "Backup completed in $(expr ${end} - ${start}) seconds."
 
 start=$(date +%s)
-runny $(/usr/local/bin/restic forget ${RESTIC_FORGET_ARGS} --prune)
+runny /usr/local/bin/restic forget ${RESTIC_FORGET_ARGS} --prune
 end=$(date +%s)
 
 logger -p user.info "Backup pruning completed in $(expr ${end} - ${start}) seconds."
 
 start=$(date +%s)
-runny $(/usr/local/bin/restic check)
+runny /usr/local/bin/restic check
 end=$(date +%s)
 
 logger -p user.info "Repository check completed in $(expr ${end} - ${start}) seconds."
--- application/restore.sh
+++ application/restore.sh
@@ -3,7 +3,7 @@
 logger -p user.info "Restoring ${RESTIC_RESTORE_ID}..."
 
 start=$(date +%s)
-runny $(/usr/local/bin/restic restore ${RESTIC_RESTORE_ID} --target ${TARGET_PATH})
+runny /usr/local/bin/restic restore ${RESTIC_RESTORE_ID} --target ${TARGET_PATH}
 end=$(date +%s)
 
 logger -p user.info "Restoration completed in $(expr ${end} - ${start}) seconds."
